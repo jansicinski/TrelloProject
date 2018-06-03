8951dd3fdfd4c1a15d676fe58b85ab5c13a78a8a
Janek <jan.sicinski@gmail.com>
2018-06-03 14:30:28 +0200

    test continued

--- src/test/java/com/janek/TrelloProject/Controllers/TrelloApiBoardControllerTest.java
+++ src/test/java/com/janek/TrelloProject/Controllers/TrelloApiBoardControllerTest.java
@@ -12,8 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.hasItems;
 
 @RunWith(SpringRunner.class)
@@ -28,7 +26,7 @@ public class TrelloApiBoardControllerTest {
 
     @Test
     public void shouldGetAllMyBoards(){
-        // TODO: 6/2/2018 TrelloApiControllerTest shouldReturnMyBoards
+        // TODO: 6/2/2018 TrelloApiControllerTest shouldReturnMyBoards, WireMock?
         //@formatter:off
         RequestSpecification given = given()
                 .port(port)
@@ -48,7 +46,19 @@ public class TrelloApiBoardControllerTest {
 
     @Test
     public void shouldSaveAllBoardsToDb(){
+        RequestSpecification given = given()
+                .port(port)
+                .log().all();
 
+        Response when = given
+                .when()
+                .post("TrelloApi/boards/saveAllToDb");
+
+        when.then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .body("name", hasItems("x", "TRNTL", "Przyjaciel SME", "Welcome Board", "Members", "Notes - devs only"));
     }
 
 }
