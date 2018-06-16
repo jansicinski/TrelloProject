4b579be46c3ee35cc215e756b735b3d09713e37f
Janek <jan.sicinski@gmail.com>
2018-06-16 13:48:01 +0200

    swagger

--- src/main/java/com/janek/TrelloProject/RestControllers/Db/TrelloDbBoardController.java
+++ src/main/java/com/janek/TrelloProject/RestControllers/Db/TrelloDbBoardController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("TrelloDb/boards/")
+@RequestMapping("TrelloDb/boards/swagger")
 public class TrelloDbBoardController {
 
     private final TrelloboardService trelloboardService;
--- src/main/java/com/janek/TrelloProject/Swagger/SwaggerController.java
+++ src/main/java/com/janek/TrelloProject/Swagger/SwaggerController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SwaggerController {
+
     @RequestMapping(value = "/documentation")
     public String index(){
         return "redirect:swagger-ui.html";
     }
+
 }
