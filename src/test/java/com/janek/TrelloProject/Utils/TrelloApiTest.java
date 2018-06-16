08cfd64edb77927e467e70810246d4c6ebf133db
Janek <jan.sicinski@gmail.com>
2018-06-16 15:21:05 +0200

    wiremock

--- src/test/java/com/janek/TrelloProject/Utils/TrelloApiTest.java
+++ src/test/java/com/janek/TrelloProject/Utils/TrelloApiTest.java
@@ -1,29 +1,52 @@
 package com.janek.TrelloProject.Utils;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import jdk.incubator.http.HttpClient;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-//import static org.junit.Assert.assertThat;
-//import static sun.nio.cs.Surrogate.is;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 public class TrelloApiTest {
 
+    private int port = 8081;
+
     @Rule
-    public WireMockRule wireMockRule = new WireMockRule(8081);
+    public WireMockRule wireMockRule = new WireMockRule(port);
 
     @Test
     public void shouldReturnMyBoards(){
-        wireMockRule.stubFor(get(urlEqualTo("/TrelloApi/boards"))
+        wireMockRule.stubFor(get(urlEqualTo("/TrelloApi/boards/"))
             .willReturn(aResponse()
                 .withHeader("Content-Type", "application/json")
-                .withBody("Hello world!")));
+                .withBody("{\n" +
+                        "\"boardId\": \"1\",\n" +
+                        "\"name\": \"catcat\"\n" +
+                        "}")));
 
-//        assertThat(testClient.get("/TrelloApi/boards").statusCode(), is(200));
+        //@formatter:off
+        RequestSpecification given = given()
+                .port(port)
+                .log().all();
+
+        Response when = given
+                .when()
+                .get("TrelloApi/boards/");
+
+        when.then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .body(containsString("{\n" +
+                        "\"boardId\": \"1\",\n" +
+                        "\"name\": \"catcat\"\n" +
+                        "}"));
+        //@formatter:on
     }
 
 }
