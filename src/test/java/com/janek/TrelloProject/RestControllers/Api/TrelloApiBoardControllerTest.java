6a2f48aa251440f3f0539cf9b142488b6afebee1
Janek <jan.sicinski@gmail.com>
2018-06-16 14:24:29 +0200

    wiremock

---
+++ src/test/java/com/janek/TrelloProject/RestControllers/Api/TrelloApiBoardControllerTest.java
@@ -0,0 +1,66 @@
+package com.janek.TrelloProject.RestControllers.Api;
+
+import com.janek.TrelloProject.Repositories.TrelloboardRepository;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TrelloApiBoardControllerTest {
+
+    @Autowired
+    TrelloboardRepository trelloboardRepository;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void shouldGetAllMyBoards(){
+        // TODO: 6/2/2018 TrelloApiControllerTest shouldReturnMyBoards, WireMock?
+        //@formatter:off
+        RequestSpecification given = given()
+                .port(port)
+                .log().all();
+
+        Response when = given
+                .when()
+                .get("TrelloApi/boards/");
+
+        when.then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .body("name", hasItems("x", "TRNTL", "Przyjaciel SME", "Welcome Board", "Members", "Notes - devs only"));
+        //@formatter:on
+    }
+
+    @Test
+    public void shouldSaveAllBoardsToDb(){
+        //@formatter:off
+        RequestSpecification given = given()
+                .port(port)
+                .log().all();
+
+        Response when = given
+                .when()
+                .post("TrelloApi/boards/saveAllToDb");
+
+        when.then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .body("name", hasItems("x", "TRNTL", "Przyjaciel SME", "Welcome Board", "Members", "Notes - devs only"));
+        //@formatter:on
+    }
+
+}
---
+++ src/test/java/com/janek/TrelloProject/RestControllers/Api/TrelloApiListControllerTest.java
@@ -0,0 +1,66 @@
+package com.janek.TrelloProject.RestControllers.Api;
+
+import com.janek.TrelloProject.Repositories.TrelloboardRepository;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TrelloApiListControllerTest {
+
+    @Autowired
+    TrelloboardRepository trelloboardRepository;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void shouldGetAllMyLists() {
+        //@formatter:off
+        RequestSpecification given = given()
+                .port(port)
+                .log().all();
+
+        Response when = given
+                .when()
+                .get("TrelloApi/lists/");
+
+        when.then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .body(containsString("5a54de08b62cff803bd7f42e=5a54de08b62cff803bd7f42b"));
+        //@formatter:on
+    }
+
+    @Test
+    public void shouldSaveAllListsToDb() {
+        //@formatter:off
+        RequestSpecification given = given()
+                .port(port)
+                .log().all();
+
+        Response when = given
+                .when()
+                .post("TrelloApi/lists/saveAllToDb");
+
+        when.then()
+                .log().all()
+                .assertThat()
+                .statusCode(HttpStatus.OK.value())
+                .body("listId", hasItems("5a54dc1ec9b64db92e6d23ca", "5a54dc1ec9b64db92e6d23cb", "5a54de08b62cff803bd7f42c"));
+        //@formatter:on
+    }
+
+}
